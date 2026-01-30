2c2beb1f676cee20bc7f61eb42f8621bd0b03bc8
Aniket Trivedi <triv.aniket04@gmail.com>
2026-01-30 09:14:41 -0800

    added Jinja2templates with static files and CSS

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), "static")
 
 templates = Jinja2Templates(directory="templates")
 
 posts: list[dict] = [
     {
         "id": 1,
-        "author": "Corey Schafer",
+        "author": "Aniket Trivedi",
         "title": "FastAPI is Awesome",
         "content": "This framework is really easy to use and super fast.",
         "date_posted": "April 20, 2025",
@@ -22,8 +25,8 @@ posts: list[dict] = [
     },
 ]
 
-@app.get("/", include_in_schema=False)
-@app.get("/posts", include_in_schema=False)
+@app.get("/", include_in_schema=False, name="home")
+@app.get("/posts", include_in_schema=False, name="posts")
 def home(request: Request):
     return templates.TemplateResponse(request, "home.html", {"posts": posts})
 
